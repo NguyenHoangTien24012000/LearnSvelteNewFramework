29a2754a3e564bc42651af3ce46360df3379b62d
NguyenTien2401 <hoangtienflytom@gmail.com>
2021-10-30 06:11:51 +0700

    Step 2

--- my-new-project/src/App.svelte
+++ my-new-project/src/App.svelte
@@ -1,71 +1,42 @@
 <script>
-  let firstName = "Tien";
-  let lastName = "Nguyen";
-  $: name = firstName + " " + lastName;
-  let color = "blue";
-  let showText = false;
-  let users = [
-    {
-      id: 1,
-      name: "John",
-    },
-    {
-      id: 2,
-      name: "Sara",
-    },
-    {
-      id: 3,
-      name: "Tien",
-    },
-  ];
+	import FeedbackList from './components/FeedbackList.svelte';
+	import FeedBackStats from './components/FeedbackStats.svelte';
+	import FeedBackForm from './components/FeedBackForm.svelte';
+	let feedback = [
+		{
+			id : 1,
+			rating : 10,
+			text : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aperiam eum eius dolorem voluptas cupiditate dolores voluptate beatae fugiat corrupti.'
+		},
+		{
+			id : 2,
+			rating : 9,
+			text : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aperiam eum eius dolorem voluptas cupiditate dolores voluptate beatae fugiat corrupti.'
+		},
+		{
+			id : 3,
+			rating: 8,
+			text : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aperiam eum eius dolorem voluptas cupiditate dolores voluptate beatae fugiat corrupti.'
+		}
+	]
+	
+	$: count = feedback.length;
+	$: average = count === 0 ? 0 : feedback.reduce((a, {rating}) => a + rating, 0) / count;
 
-  const toggle = () => {
-    color = color === "blue" ? "red" : "blue";
-  };
-  const toggleText = () => {
-    showText = !showText;
-  };
-  const addUser = () =>{
-	  users = [...users, {id : 4, name : 'Tung'}];
-  }
+	const deleteFeedback = (e) =>{
+		console.log(e.detail)
+		const itemId = e.detail;
+		feedback = feedback.filter(item => item.id != itemId);
+	}
 </script>
 
-<main>
-  <h1 style="color: {color};">Hello {name}!</h1>
-  {#if showText}
-    <p>
-      Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-      how to build Svelte apps.
-    </p>
-  {:else}
-    <p>No Text</p>
-  {/if}
-	{#each users as user (user.id)}
-		<h3>{user.id}: {user.name}</h3>
-	{/each}
-  <button on:click={toggleText}>Show Text</button>
-  <button on:click={toggle}>Change Color</button>
-  <button on:click={addUser}>Add User</button>
+<main class="container">
+	<FeedBackForm />
+	<FeedBackStats {count} {average} />
+  <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
+
 </main>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
 </style>
---
+++ my-new-project/src/components/FeedBackForm.svelte
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+
+<form>
+    <div class="input-group">
+        <input type="text" placeholder="Tell us something that kepps you coming back">
+        <button>Send</button>
+    </div>
+</form>
